8f123d2833ac8c4dd7f78bed51c1334148040945
marcobrunettionqor <marco.brunetti@onqor.co.uk>
2023-10-31 18:01:07 +0000

    git ignore

--- storefront-child/functions.php
+++ storefront-child/functions.php
@@ -19,3 +19,12 @@ function my_child_theme_enqueue_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'my_child_theme_enqueue_styles' );
+
+
+function vc_remove_wp_ver_css_js( $src ) {
+    if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+    }
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
