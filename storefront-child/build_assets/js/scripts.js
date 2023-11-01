84f36d3c46d96da61417a4090083241778b48362
marcobrunettionqor <marco.brunetti@onqor.co.uk>
2023-11-01 18:56:03 +0000

    header - first part

--- storefront-child/build_assets/js/scripts.js
+++ storefront-child/build_assets/js/scripts.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
+    // Hamburger Menu
 
+    $('.header__wrapper__hamburger').on('click', function() {
+        $('.mobile-menu').toggleClass('open');
+        $('.close').toggleClass('show');
+        $('.hamburger').toggleClass('hide');
+        $('.header').toggleClass('menu-mobile');
+    });
+
+    // Menu Mobile 
+
+    $('.menu-item-has-children').on('click', function(e) {
+        e.preventDefault();
+        var $clicked = $(this);
+
+        $('.menu-item-has-children').not($clicked).each(function() {
+            $(this).removeClass('open');
+        });
+
+        $clicked.toggleClass('open');
+    });
 
 });
